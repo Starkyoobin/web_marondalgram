573d5c553cc390b89c8b148883d71e20a7627771
DESKTOP-8NADJPO\yb010 <yb010626@gmail.com>
2021-09-10 10:51:41 +0900

    [김유빈] -마론달그램 로그인 화면 (404 error)

---
+++ src/main/java/com/starkyb/marondalgram/user/UserController.java
@@ -0,0 +1,14 @@
+package com.starkyb.marondalgram.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@GetMapping("/signin_view")
+	public String signinView() {
+		return "user/signIn";
+	}
+}
